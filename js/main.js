988aa9f51108b19aa10bfacd3d886f8ea8fea08b
cynthialu42 <clu2334@gmail.com>
2018-03-23 14:02:02 -0500

    added input functionality and ratings

--- js/main.js
+++ js/main.js
@@ -4,31 +4,53 @@ $(document).ready(function(){
 
     var topics = ["cat", "bird", "dog", "obama"];
 
-    for (let i = 0; i < topics.length; i++){
-        let bDiv = $('<button>');
-        bDiv.attr("name", topics[i]);
-        bDiv.text(topics[i]);
-        $('.button-section').append(bDiv);
+    function createButtons(){
+        $('.button-section').empty();
+
+        for (let i = 0; i < topics.length; i++){
+            let bDiv = $('<button>');
+            bDiv.attr("data-name", topics[i]);
+            bDiv.addClass("gif-btn");
+            bDiv.text(topics[i]);
+            $('.button-section').append(bDiv);
+        }
     }
-    $('button').on('click', function(){
-        var query = $(this).attr("name");
+    
+
+    $('.submit').on('click',function(event){
+
+        event.preventDefault();
+        let val = $('#searchTerm').val().trim();
+        topics.push(val);
+        $('#searchTerm').val("");
+        createButtons();
+    });
+
+    $(document).on("click", ".gif-btn", displayGifs);
+
+    function displayGifs(){
+        var query = $(this).attr("data-name");
         var limit = "10";
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit;
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
-            console.log(response); 
+            console.log(response);
             for (let i = 0; i < response.data.length; i++){
                 let gif = $('<img>');
+                let rating = $('<p>');
                 let section = $('<div class = "gif" >');
+
                 gif.attr("src",response.data[i].images.fixed_height.url);
-                section.append(gif);
+                rating.text(response.data[i].rating);
+
+                section.append(rating).append(gif);
                 $('.js-gifs').append(section);
             }
         });
         $('.js-gifs').empty();
-    });
+    };
 
-
+    createButtons();
 });

33100b3f047050a61316ff23f984a0fbd2fc8d76
cynthialu42 <clu2334@gmail.com>
2018-03-23 17:29:43 -0500

    comments, jquery loop, footer, styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     var apiKey = "5jIbhfm2DTsEsvSRNL8beHu0ogreChsM";
    
-
     var topics = ["cat", "bird", "dog", "obama","giraffe", "bts", "camel", "elephant", "gerbil"];
 
+    // Generate the buttons
     function createButtons(){
         $('.button-section').empty();
 
@@ -15,8 +15,57 @@ $(document).ready(function(){
             $('.button-section').append(bDiv);
         }
     }
-    
 
+    // Pause or animate gif
+    function animateGifs(){
+        var state = $(this).attr('data-state');
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    }
+
+    // Change the title based on selection
+    function displayTitle(query){
+        $('.title').empty().text("WOW " + query.toUpperCase() + " GIFS!!!");
+    }
+
+    // Show the gifs on the page
+    function displayGifs(){
+        var query = $(this).attr("data-name");
+
+        displayTitle(query);
+
+        var limit = "10";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            let data = response.data;
+            data.forEach(function(result){
+                let gif = $('<img>');
+                let rating = $('<p>');
+                let section = $('<div class = "still-gif" >');
+
+                let stillGif = result.images.fixed_height_still.url;
+                let movingGif = result.images.fixed_height.url
+                let state = "still";
+                gif.attr("src",stillGif).attr("data-still", stillGif).attr("data-animate", movingGif).attr("data-state", state).addClass("gif-img");
+                rating.text("Rated: " + result.rating.toUpperCase());
+                section.append(rating).append(gif);
+                $('.js-gifs').append(section);
+            });
+          
+        });
+        $('.js-gifs').empty();
+    }
+    
+    // Submit event for new button
     $('.submit').on('click',function(event){
 
         event.preventDefault();
@@ -26,65 +75,11 @@ $(document).ready(function(){
         createButtons();
     });
 
+    // Event for button to generate gif
     $(document).on("click", ".gif-btn", displayGifs);
-    $(document).on("click", ".gif-img", moveGifs);
-    $('.test').on("click",function(){
-        console.log('here');
-        var state = $(this).attr('data-state');
-        console.log(state);
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-    });
 
-    function moveGifs(){
-        console.log('here');
-        var state = $(this).attr('data-state');
-        console.log(state);
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-    }
-
-    function displayTitle(query){
-        $('.title').empty().text("WOW " + query.toUpperCase() + " GIFS!!!");
-
-    }
-
-    function displayGifs(){
-        var query = $(this).attr("data-name");
-        displayTitle(query);
-        var limit = "10";
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit;
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-            for (let i = 0; i < response.data.length; i++){
-                let gif = $('<img>');
-                let rating = $('<p>');
-                let section = $('<div class = "still-gif" >');
-
-                let stillGif = response.data[i].images.fixed_height_still.url;
-                let movingGif = response.data[i].images.fixed_height.url
-                let state = "still";
-                gif.attr("src",stillGif).attr("data-still", stillGif).attr("data-animate", movingGif).attr("data-state", state).addClass("gif-img");
-                rating.text("Rated: " + response.data[i].rating);
-                section.append(rating).append(gif);
-                $('.js-gifs').append(section);
-            }
-        });
-        $('.js-gifs').empty();
-    };
+    // Event for pausing or animating a gif
+    $(document).on("click", ".gif-img", animateGifs);
 
     createButtons();
 });

564a736566fe50bfbde6eb440534d18c63514e8f
cynthialu42 <clu2334@gmail.com>
2018-03-23 14:33:00 -0500

    added pausing gifs

--- js/main.js
+++ js/main.js
@@ -27,7 +27,32 @@ $(document).ready(function(){
     });
 
     $(document).on("click", ".gif-btn", displayGifs);
+    $(document).on("click", ".gif-img", moveGifs);
+    $('.test').on("click",function(){
+        console.log('here');
+        var state = $(this).attr('data-state');
+        console.log(state);
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 
+    function moveGifs(){
+        console.log('here');
+        var state = $(this).attr('data-state');
+        console.log(state);
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    }
     function displayGifs(){
         var query = $(this).attr("data-name");
         var limit = "10";
@@ -40,11 +65,14 @@ $(document).ready(function(){
             for (let i = 0; i < response.data.length; i++){
                 let gif = $('<img>');
                 let rating = $('<p>');
-                let section = $('<div class = "gif" >');
+                let section = $('<div class = "still-gif" >');
+//  <img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" data-state="still" class="gif">
 
-                gif.attr("src",response.data[i].images.fixed_height.url);
+                let stillGif = response.data[i].images.fixed_height_still.url;
+                let movingGif = response.data[i].images.fixed_height.url
+                let state = "still";
+                gif.attr("src",stillGif).attr("data-still", stillGif).attr("data-animate", movingGif).attr("data-state", state).addClass("gif-img");
                 rating.text(response.data[i].rating);
-
                 section.append(rating).append(gif);
                 $('.js-gifs').append(section);
             }

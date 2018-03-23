f4eb881f75d97a9121a8cdbe4eaeee8d5f7edc50
cynthialu42 <clu2334@gmail.com>
2018-03-23 13:19:55 -0500

    added buttons to trigger new things

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
     var apiKey = "5jIbhfm2DTsEsvSRNL8beHu0ogreChsM";
-    var query = $('button').attr("name");
-    var limit = "10";
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit;
+   
 
+    var topics = ["cat", "bird", "dog", "obama"];
+
+    for (let i = 0; i < topics.length; i++){
+        let bDiv = $('<button>');
+        bDiv.attr("name", topics[i]);
+        bDiv.text(topics[i]);
+        $('.button-section').append(bDiv);
+    }
     $('button').on('click', function(){
-        console.log("here");
+        var query = $(this).attr("name");
+        var limit = "10";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit;
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -19,6 +27,7 @@ $(document).ready(function(){
                 $('.js-gifs').append(section);
             }
         });
+        $('.js-gifs').empty();
     });
 
 
